8f3bc22907900ef168c1e4a7411654107d98b645
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-04-03 13:23:14 +0300

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,25 @@
+﻿class Televizyon
+{
+    public int SesSeviyesi { get; set; }
+    public double EkranBoyutu { get; set; }
+    public string GoruntuTeknolojisi { get; set; }
+
+    bool gucacik = false;
+    int kanalNo = 1;
+    public void GucAc()
+    {
+        gucacik = true;
+    }
+    public void GucKapat()
+    {
+        gucacik = false;
+    }
+    public void KanalDegistir(int kanalNo)
+    {
+        this.kanalNo = kanalNo;
+    }
+    public int SesSeviyesiOku()
+    {
+        return SesSeviyesi;
+    }
+}
